68bf4bfde55c3edd8edc313018cf5e56539e32c7
daviribeiro <luminuszz43@gmail.com>
2023-05-25 12:13:49 -0300

    fix bugs

--- src/workers/utils/createLogger.ts
+++ src/workers/utils/createLogger.ts
@@ -2,5 +2,9 @@ import { pino } from 'pino';
 import PinoPretty from 'pino-pretty';
 
 export const createLogger = (value?: string) => {
-  return pino(PinoPretty({ colorize: true }));
+  return pino({
+    ...PinoPretty({ colorize: true }),
+    level: 'error',
+    name: value,
+  });
 };
